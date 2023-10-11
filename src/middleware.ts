6582cf63668a6996b0c6cebd79b54966557f5c76
Tom Davies <tvdavies@gmail.com>
2023-10-11 13:39:28 +0100

    replace URLs in styles

--- src/middleware.ts
+++ src/middleware.ts
@@ -61,12 +61,25 @@ export async function middleware(request: NextRequest) {
     html = html.replace(/<head[^>]*>/, (match) => `${match}${baseTag}`);
 
     html = html.replace(/(href|src|srcset)="([^"]*)"/g, (match, p1, p2) => {
-      if (p2.startsWith("http")) {
+      if (p2.startsWith(url.origin)) {
         return `${PROXY_URL}/${match}`;
       }
 
       if (p2.startsWith("/")) {
         return `${p1}="${PROXY_URL}/${url.origin + p2}"`;
+      }
+
+      return match;
+    });
+
+    // Replace all relative URLs in styles with absolute URLs
+    html = html.replace(/url\("?([^)]*)"?\)/g, (match, p1) => {
+      if (p1.startsWith(url.origin)) {
+        return `url("${PROXY_URL}/${p1}")`;
+      }
+
+      if (p1.startsWith("/")) {
+        return `url("${PROXY_URL}/${url.origin + p1}")`;
       }
 
       return match;
@@ -81,6 +94,28 @@ export async function middleware(request: NextRequest) {
     html = html.replace("</body>", `${scriptTag}</body>`);
 
     return new NextResponse(html, {
+      status: response.status,
+      headers: responseHeaders,
+    });
+  }
+
+  // If content is CSS, replace all relative URLs with absolute URLs
+  if (responseHeaders.get("content-type")?.includes("text/css")) {
+    let css = await response.text();
+
+    css = css.replace(/url\("?([^)]*)"?\)/g, (match, p1) => {
+      if (p1.startsWith(url.origin)) {
+        return `url("${PROXY_URL}/${p1}")`;
+      }
+
+      if (p1.startsWith("/")) {
+        return `url("${PROXY_URL}/${url.origin + p1}")`;
+      }
+
+      return match;
+    });
+
+    return new NextResponse(css, {
       status: response.status,
       headers: responseHeaders,
     });
